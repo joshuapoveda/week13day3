83044e8b07c025bcfd2b0ce8c37efc23041aeb8c
Joshua Poveda <joshuapoveda9@gmail.com>
2023-05-22 00:24:17 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+import SearchBar from "./components/SearchBar";
+import EmployeeList from "./components/EmployeeList";
+import EmployeePage from "./components/EmployeePage";
+import Homepage from "./components/Homepage";
+const employees = require("./models/Employees");
+
+function App() {
+  return (
+    <div className="app">
+      <div className="homepage">
+        <Homepage></Homepage>
+        <div className="header">
+          <Header></Header>
+        </div>
+        <div className="search-bar">
+          <SearchBar></SearchBar>
+        </div>
+        <div className="employee-list">
+          <EmployeeList employees={employees}></EmployeeList>
+        </div>
+      </div>
+      <div className="employee-page">
+      <Routes>
+            {employees.map((i, index) => (
+              <Route
+                key={index}
+                path={`/${index}`}
+                element={<EmployeePage index={index} employees={employees} />}
+              />
+            ))}
+          </Routes>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/models/Employees.js
@@ -0,0 +1,53 @@
+const employees = [
+    {
+        name: 'James King',
+        title: 'President',
+        officeNum: 1234567,
+        mobileNum: 1234567,
+        smsNum: 1234567,
+        email: '123@gmail.com'
+    },
+    {
+        name: 'Julie Taylore',
+        title: 'VP of Marketing',
+        officeNum: 89101112,
+        mobileNum: 89101112,
+        smsNum: 89101112,
+        email: '456@gmail.com'
+    },
+    {
+        name: 'Eugene Lee',
+        title: 'CFO',
+        officeNum: 13141516,
+        mobileNum: 13141516,
+        smsNum: 13141516,
+        email: '679@gmail.com'
+    },
+    {
+        name: 'John Williams',
+        title: 'VP of Engineering',
+        officeNum: 17181920,
+        mobileNum: 17181920,
+        smsNum: 17181920,
+        email: '1011@gmail.com'
+    },
+    {
+        name: 'Ray Moore',
+        title: 'VP of sales',
+        officeNum: 21222324,
+        mobileNum: 21222324,
+        smsNum: 21222324,
+        email: '1213@gmail.com'
+    },
+    {
+        name: 'Paul Jones',
+        title: 'QA Manager',
+        officeNum: 25262728,
+        mobileNum: 25262728,
+        smsNum: 25262728,
+        email: '1415@gmail.com'
+    }
+
+]
+
+module.exports = employees
